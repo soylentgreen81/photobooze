c107752022d00c309ca9f276fc4874543a774276
soylentgreen81 <stefan.beukmann@googlemail.com>
2018-10-28 15:05:34 +0000

    Copied capture example code

--- booze.py
+++ booze.py
@@ -5,10 +5,12 @@ from flask import (
         jsonify,
         render_template,
         Response,
-        stream_with_context
+        stream_with_context,
+        send_file
         )
 
 import gphoto2 as gp
+import os
 
 
 booze = Flask(__name__, static_folder="static", template_folder="templates")
@@ -31,10 +33,19 @@ def preview():
 
 @booze.route("/click")
 def click():
-    return abort(404)
-
-
+    camera = gp.check_result(gp.gp_camera_new())
+    gp.check_result(gp.gp_camera_init(camera))
+    print('Capturing image')
+    file_path = gp.check_result(gp.gp_camera_capture(camera, gp.GP_CAPTURE_IMAGE))
+    print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
+    target = os.path.join('/tmp', file_path.name)
+    print('Copying image to', target)
+    camera_file = gp.check_result(gp.gp_camera_file_get(
+            camera, file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL))
+    gp.check_result(gp.gp_file_save(camera_file, target))
+    gp.check_result(gp.gp_camera_exit(camera))
+    return send_file(target, mimetype='image/jpeg')
 
 
 if __name__ == "__main__":
-    booze.run ( host = "localhost", port = "8000", debug = "True" )
+    booze.run (  host="0.0.0.0", port = "8000", debug = "True" )
