f928215906e0304a5687005f0fb7c75da9c51724
soylentgreen81 <stefan.beukmann@googlemail.com>
2018-10-29 21:12:07 +0000

    Merge with gphoto-cffi branch

--- booze.py
+++ booze.py
@@ -10,7 +10,8 @@ from flask import (
         )
 
 import gphoto2 as gp
-import os
+from os import listdir
+from os.path import join
 
 import threading
 
@@ -20,50 +21,43 @@ imagedir = "/home/alarm/images/"
 
 cameraSemaphore = threading.Semaphore(1)
 
-def preview_stream():
-    # init gphoto2 here
-    gphoto = "Hallo Welt"
-    while True:
-        yield gphoto
-
 
 @booze.route("/", methods = ["GET"])
 def index():
     return render_template("index.html")
 
-@booze.route("/preview", methods = ["GET"])
-def preview():
-    return abort(404)
-    return Response(response=stream_with_context(preview_stream()), mimetype="text/plain")
 
 @booze.route("/pictures", methods = ["GET"])
 def getPictures():
-    data = []
-    for root, dirs, files in os.walk(imagedir):
-        for filename in files:
-            data.append({'pictureurl':'/pictures/' + filename})
+    data = [ 
+            {'pictureurl': '/pictures/'+p} 
+            for p in listdir(imagedir)
+    ]
     return jsonify(data)
     
 @booze.route("/pictures/<filename>")
 def getPicture(filename):
-    image = os.path.join(imagedir, filename)
+    image = join(imagedir, filename)
     return send_file(image, mimetype="image/jpeg")
+
+
 
 @booze.route("/pictures", methods=["POST"])
 def postPicture():
-    with cameraSemaphore:
+    with cameraSemaphore: 
         camera = gp.check_result(gp.gp_camera_new())
         gp.check_result(gp.gp_camera_init(camera))
-        print('Capturing image')
+        config = gp.check_result(gp.gp_camera_get_config(camera))
+        capture_target = gp.check_result(gp.gp_widget_get_child_by_name(config, 'capturetarget'))
+        value = gp.check_result(gp.gp_widget_get_choice(capture_target, 1))
+        gp.check_result(gp.gp_widget_set_value(capture_target, value))
+        gp.check_result(gp.gp_camera_set_config(camera, config))
         file_path = gp.check_result(gp.gp_camera_capture(camera, gp.GP_CAPTURE_IMAGE))
-        print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
-        target = os.path.join(imagedir, file_path.name)
-        print('Copying image to', target)
+        target = join(imagedir, file_path.name)
         camera_file = gp.check_result(gp.gp_camera_file_get(camera, file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL))
         gp.check_result(gp.gp_file_save(camera_file, target))
         gp.check_result(gp.gp_camera_exit(camera))
         return jsonify({'pictureurl':'/pictures/' + file_path.name})
-
 
 if __name__ == "__main__":
     booze.run (  host="0.0.0.0", port = "8000", debug = "True" )
--- static/booze.js
+++ static/booze.js
@@ -1,1 +1,15 @@
 "use strict";
+document.getElementById("trigger").addEventListener("click",snap);
+
+function snap(){
+    console.log("Taking a picture...");
+    fetch('/pictures',{method:"POST"})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+         console.log(data.pictureurl);
+         document.getElementById("image").src = data.pictureurl;
+    });
+
+}
