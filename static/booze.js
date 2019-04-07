86146d25a34e0d87a2247ccacda0d3ae38538162
Stefan Beukmann <stefan.beukmann@googlemail.com>
2019-04-07 01:03:04 +0000

    final countdown

--- static/booze.js
+++ static/booze.js
@@ -1,15 +1,58 @@
 "use strict";
-document.getElementById("trigger").addEventListener("click",snap);
+const btn = document.getElementById("trigger");
+const image = document.getElementById("image");
+const counter = document.getElementById("counter");
+btn.addEventListener("click",countdownTimer);
+
+function countdownTimer(){
+	// weg mit listener
+	btn.removeEventListener("click",countdownTimer);
+	let timeLeft = 5;
+	btn.value = 'Noch ' + timeLeft + ' Sekunden';
+	image.style.background = '';
+	counter.classList.add('animated'); 
+	counter.classList.add('intensifies'); 
+	counter.innerHTML = timeLeft;
+	const downloadTimer = setInterval(function(){
+		timeLeft--;
+	    console.log(`Noch ${timeLeft} Sekunden`);
+	    btn.value = `Noch ${timeLeft} Sekunden`;
+		counter.innerHTML = timeLeft;
+		if (timeLeft <= 0){
+			clearInterval(downloadTimer);
+			counter.innerHTML = '';
+			counter.classList.remove('animated'); 
+			counter.classList.remove('intensifies'); 
+			snap();
+		}
+	}, 1000);
+
+
+
+}
+
 
 function snap(){
-    let image = document.getElementById("image");
     console.log("Taking a picture...");
-    image.src ="/static/spinner.svg";
+    image.style.background ="url(/static/spinner.svg) center no-repeat";
     fetch('/api/v1/pictures',
        {method:"POST"}
     )
       .then(response=> response.json() )
-      .then(json=> image.src = json.scaled)
+      .then(json => {
+		  image.style.background ="url(" + json.scaled + ") center no-repeat";
+			btn.addEventListener("click",countdownTimer);
+			btn.value = 'Click!';
+			setTimeout(function(){ 
+				image.classList.add('animated'); 
+				image.classList.add('intensifies'); 
+				setTimeout(function(){ 
+					image.style.background = '';
+					image.classList.remove('animated'); 
+					image.classList.remove('intensifies'); 
+				},2000)
+			},5000)
+	  })
       .catch(error => image.src='/static/broken.png');
 
 }
