ca527b6e793674f1ec2d0158cdfe2940122e3a35
Stefan Beukmann <stefan.beukmann@googlemail.com>
2019-08-24 19:57:26 +0000

    randomized slideshow

--- static/slideshow.js
+++ static/slideshow.js
@@ -9,11 +9,28 @@ var slides = new Vue({
             fetch("/api/v1/pictures")
                 .then(res => res.json())
                 .then(res => {
-                    this.images=res;
+                    this.images=this.shuffle(res);
                     
                 }
             );
         },
+        shuffle: function shuffle(array) {
+            let currentIndex = array.length, temporaryValue, randomIndex;
+            
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
+            
+                // Pick a remaining element...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+            
+                // And swap it with the current element.
+                temporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+            }
+            return array;
+        }
        
     },
     mounted: function () {
@@ -32,11 +49,9 @@ var slides = new Vue({
         else{
             //Hacky hacky!
             this.slideshow.stop();
-            const currentSlide = this.slideshow.state.slideVisibleNum;
-            console.log(`stopped at ${currentSlide}`);
             this.slideshow.state.slideVisibleEl.classList.remove(this.slideshow.props.cssPrefix + '--has-kenBurnsFx');
             this.slideshow.init();
-            this.slideshow.state.slideVisibleNum = currentSlide;
+            
         }
     }
 });
